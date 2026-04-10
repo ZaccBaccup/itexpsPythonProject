04897de54d5f8d0b6748f9560d2a97502330b20f
Zlrobbins9 <heccmedaddy1234@gmail.com>
2026-04-10 16:22:45 -0500

    Fixed FollowedUp flag not represented in CSV
    - Added isFollowedUp flag to testOutputJobs.csv
    - changed job application csv parsing to take 7 parameters, the 7th one is converted from a string to an int and then to a boolean.
    -changed the job application class to input the followedup flag, hold it, and print it out with both the print everything() and the new getFollowedUpStatus()
    - commented out code that prints every object loaded for csv

--- JobApplicationClass.py
+++ JobApplicationClass.py
@@ -6,11 +6,12 @@ from datetime import date
 
 class JobApplication:
 
-    def __init__(self,companyName,jobTitle,appDay,appMonth,appYear,jobLink):
+    def __init__(self,companyName,jobTitle,appDay,appMonth,appYear,jobLink,isFollowedUp):
         self.__companyName = companyName
         self.__jobTitle = jobTitle
         self.__applyDate = date(appYear,appMonth,appDay)
         self.__jobLink = jobLink
+        self.__isFollowedUp = isFollowedUp
         # self.__AppDay = AppDay
         # self.__AppMonth = AppMonth
         # self.__AppYear = AppYear
@@ -26,5 +27,8 @@ class JobApplication:
     def GetJobLink(self):
         return self.__jobLink
     
+    def GetFollowedUpStatus(self):
+        return self.__isFollowedUp
+    
     def GetEverything(self):
-        return [self.__companyName, self.__jobTitle, self.__applyDate.day, self.__applyDate.month, self.__applyDate.year, self.__jobLink]
+        return [self.__companyName, self.__jobTitle, self.__applyDate.day, self.__applyDate.month, self.__applyDate.year, self.__jobLink, self.__isFollowedUp]
--- JobApplicationGet.py
+++ JobApplicationGet.py
@@ -2,11 +2,10 @@ from JobApplicationClass import JobApplication
 import csv
 
 #self,CompanyName,JobTitle,AppDay,AppMonth,AppYear,JobLink
-NewJob = JobApplication("Among Us", "Impsoster", 2,5,2025,"N/A")
+NewJob = JobApplication("Among Us", "Impsoster", 2,5,2025,"N/A",False)
 
 newJobDate = NewJob.GetApplyDate()
 print(newJobDate.year, newJobDate.month, newJobDate.day)
-
 
 fileToRead = "TestOutputJobs.csv"
 
@@ -18,13 +17,14 @@ with open(fileToRead, 'r') as csvfile:
     next(csvreader) #to add consistency, the column names are the first row.
     for curRow in csvreader:#adds the row into a JobApplication Object
         try:
-            jobApplicationList.append(JobApplication(curRow[0], curRow[1],int(curRow[2]), int(curRow[3]),int(curRow[4]), curRow[5]))
+            jobApplicationList.append(JobApplication(curRow[0], curRow[1],int(curRow[2]), int(curRow[3]),int(curRow[4]), curRow[5],bool(int(curRow[6]))))
         except IndexError:
-            print("Uh oh! This job application only has", len(curRow), "entries when it should have 6!")
+            print("Uh oh! This job application only has", len(curRow), "entries when it should have 7!")
 
 
 
-for curJA in jobApplicationList:
-    print(curJA.GetEverything())
+#prints out all files read in, use for debugging
+# for curJA in jobApplicationList:
+#     print(curJA.GetEverything())
 
 
