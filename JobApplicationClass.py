26fa5c245fa9be15de68aad3de0cdf54d267fff8
Zlrobbins9 <heccmedaddy1234@gmail.com>
2026-04-20 13:00:30 -0500

    Improved JobApplication class, added
    - Created New JobAppList class that holds all of the jobapplication objects
    - jobapplist init function holds text parsing, so all it needs now is the txt file to parse
    - jobapplicationGet.py is no longer required, but is left for demonstration purposes

--- JobApplicationClass.py
+++ JobApplicationClass.py
@@ -1,8 +1,38 @@
 
 #CompanyName,JobTitle,AppDay,AppMonth,AppYear,JobLink
+import csv
 
-from datetime import date
 
+from datetime import datetime
+
+class JobAppList:
+    
+
+
+    def __init__(self, fileToRead):
+        self.__jobList = []
+
+        print("Reading from file", fileToRead, "utilizing the csv library...")
+        with open(fileToRead, 'r') as csvfile:
+            csvreader = csv.reader(csvfile)  # Reader object
+            next(csvreader) #to add consistency, the column names are the first row.
+            for curRow in csvreader:#adds the row into a JobApplication Object
+                try: #ID,companyName,jobTitle,appDay,appMonth,appYear,jobLink,isFollowedUp
+                    self.__jobList.append(JobApplication(curRow[0], curRow[1],curRow[2], datetime.strptime(curRow[3],"%m-%d-%Y"),curRow[4],bool(int(curRow[5])))) #int(curRow[3]),int(curRow[4]), curRow[5],bool(int(curRow[6]))))
+                except IndexError:
+                    print("Uh oh! This job application only has", len(curRow), "entries when it should have 6!")
+        
+
+    def getJobList(self):
+        return self.__jobList
+    
+
+    def getJobListasStr(self):
+        strList = []
+        for i in self.__jobList:
+            strList.append(i.GetEverythingAsStr())
+
+        return strList
 
 class JobApplication:
 
@@ -31,3 +61,6 @@ class JobApplication:
     
     def GetEverything(self):
         return [self.__ID, self.__companyName, self.__jobTitle, str(self.__applyDate.month)+"-"+str(self.__applyDate.day)+"-"+str(self.__applyDate.year), self.__jobLink, self.__isFollowedUp]
+    
+    def GetEverythingAsStr(self):
+        return "Job Number "+  self.__ID + ": company Name: " + self.__companyName + ", title:" + self.__jobTitle + ", applied on:  " + str(self.__applyDate.month)+"-"+str(self.__applyDate.day)+"-"+str(self.__applyDate.year) + ", link is: ", self.__jobLink
--- JobApplicationGet.py
+++ JobApplicationGet.py
@@ -1,6 +1,6 @@
-from JobApplicationClass import JobApplication
+from JobApplicationClass import JobApplication, JobAppList
 from datetime import datetime
-import csv
+# import csv
 
 #self,CompanyName,JobTitle,AppDay,AppMonth,AppYear,JobLink
 NewJob = JobApplication(-1,"Among Us", "Impsoster", datetime.strptime("2-5-2025","%m-%d-%Y"),"N/A",False)
@@ -8,20 +8,9 @@ NewJob = JobApplication(-1,"Among Us", "Impsoster", datetime.strptime("2-5-2025"
 newJobDate = NewJob.GetApplyDate()
 print(newJobDate.year, newJobDate.month, newJobDate.day)
 
-fileToRead = "JobApplicationsTest.csv"
+NEOjobAppList = JobAppList("JobApplicationsTest.csv")
 
-jobApplicationList = []
-
-print("Reading from file", fileToRead, "utilizing the csv library...")
-with open(fileToRead, 'r') as csvfile:
-    csvreader = csv.reader(csvfile)  # Reader object
-    next(csvreader) #to add consistency, the column names are the first row.
-    for curRow in csvreader:#adds the row into a JobApplication Object
-        try: #ID,companyName,jobTitle,appDay,appMonth,appYear,jobLink,isFollowedUp
-            jobApplicationList.append(JobApplication(curRow[0], curRow[1],curRow[2], datetime.strptime(curRow[3],"%m-%d-%Y"),curRow[4],bool(int(curRow[5])))) #int(curRow[3]),int(curRow[4]), curRow[5],bool(int(curRow[6]))))
-        except IndexError:
-            print("Uh oh! This job application only has", len(curRow), "entries when it should have 6!")
-
+print("my list of applied jobs is: ", NEOjobAppList.getJobListasStr())
 
 
 #prints out all files read in, use for debugging
