cd13d399c0341aac9d1463d9f6102bf815f722a2
Zlrobbins9 <heccmedaddy1234@gmail.com>
2026-04-20 15:58:52 -0500

    Fixed CheckFollowUp.py
    - Fixed bug where date would return in the wrong format
    - fixed bug where the  joblink column would dissapear (NOTE: That column cannot have N/A as a value.
    - Fixed formatting and added row for date proximity testing.

--- CheckFollowUp.py
+++ CheckFollowUp.py
@@ -7,9 +7,6 @@ CSV_FILE = "JobApplicationsTest.csv"
 # Load CSV
 df = pd.read_csv(CSV_FILE)
 
-# Ensure correct types
-df['FollowedUp'] = df['FollowedUp'].astype(str).str.lower() == 'true'
-
 # Convert date column (adjust format if needed)
 df['AppDate(M-D-Y)'] = pd.to_datetime(df['AppDate(M-D-Y)'], errors='coerce')
 
@@ -18,9 +15,11 @@ cutoff_date = datetime.now() - timedelta(days=7)
 
 # Filter rows needing follow-up
 pending = df[
-    (df['FollowedUp'] == False) &
+    (df['FollowedUp'] == 0) &
     (df['AppDate(M-D-Y)'] < cutoff_date)
 ]
+
+# print(pending)
 
 if pending.empty:
     print("No follow-ups needed!")
@@ -29,20 +28,24 @@ else:
 
     for index, row in pending.iterrows():
         print("----")
-        print(f"Company: {row.get('company', 'N/A')}")
-        print(f"Role: {row.get('role', 'N/A')}")
-        print(f"Applied Date: {row['AppDate(M-D-Y)'].date()}")
+        print(f"Company: {row.get('CompanyName', 'N/A')}")
+        print(f"Role: {row.get('JobTitle', 'N/A')}")
+        print(f"Applied Date: {row.get('AppDate(M-D-Y)', 'N/A').strftime("%m-%d-%Y")}")
 
         user_input = input("Did you follow up? (y/n): ").strip().lower()
 
         if user_input == 'y':
-            df.at[index, 'FollowedUp'] = True
+            df.at[index, 'FollowedUp'] = 1
             print("Marked as followed up.\n")
         else:
             print("Reminder will stay.\n")
+        
 
 # Save updated CSV
-df['FollowedUp'] = df['FollowedUp'].astype(bool)
+df['FollowedUp'] = df['FollowedUp'].astype(int)  #convert to int
+
+df['AppDate(M-D-Y)'] = df['AppDate(M-D-Y)'].dt.strftime("%m-%d-%Y")
+
 df.to_csv(CSV_FILE, index=False)
 
-print("File updated successfully.")
+# print("File updated successfully.")
